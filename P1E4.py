17b856b81293f77426f44d6fd57b2f669046b263
Luciano Barletta <lushobarlett@gmail.com>
2023-02-05 10:45:15 -0300

    finish exercise

--- P1E4.py
+++ P1E4.py
@@ -1,7 +1,7 @@
 from itertools import chain
 from agent import Agent
 from node import Node
-from node_queue import DFSNodeQueue
+from node_queue import DFSNodeQueue, AstarNodeQueue
 from visit_registry import VisitParent
 
 def t(grid, i, j):
@@ -25,15 +25,17 @@ def swap_right(pos, grid):
     if pos % 3 < 2:
         yield t(grid, pos, pos + 1)
 
-def expand_grid(grid):
+def expand_grid(grid, h):
     z = grid.index(0)
 
-    return chain(
+    grid_states = chain(
         swap_up(z, grid),
         swap_right(z, grid),
         swap_down(z, grid),
         swap_left(z, grid),
     )
+
+    return ((s, 1, h(s)) for s in grid_states)
 
 initial = (
     2, 8, 3,
@@ -47,9 +49,10 @@ final = (
     7, 6, 5,
 )
 
-grid_queue = DFSNodeQueue(expand_grid)
 visit_strategy = VisitParent()
 configurations = 9*8*7*6*5*4*3*2*1
+
+grid_queue = DFSNodeQueue(expand_grid)
 agent = Agent(initial, final, grid_queue, visit_strategy, max_depth=configurations)
 path, cost = agent.go()
 
@@ -65,6 +68,18 @@ def h1(grid):
     return sum(int(i != grid.index(x)) for i, x in enumerate(final) if x != 0)
 
 
+grid_queue = AstarNodeQueue(expand_grid, h1)
+agent = Agent(initial, final, grid_queue, visit_strategy)
+path, cost = agent.go()
+
+if path is None:
+    print("No hay camino")
+    exit(1)
+
+print(f"Movimientos totales: {len(path) - 1}")
+print(f"Costo: {cost}")
+
+
 def h2(grid):
 
     def mdist(i, j):
@@ -73,3 +88,15 @@ def h2(grid):
         return hdist + vdist
 
     return sum(mdist(i, grid.index(x)) for i, x in enumerate(final) if x != 0)
+
+
+grid_queue = AstarNodeQueue(expand_grid, h2)
+agent = Agent(initial, final, grid_queue, visit_strategy)
+path, cost = agent.go()
+
+if path is None:
+    print("No hay camino")
+    exit(1)
+
+print(f"Movimientos totales: {len(path) - 1}")
+print(f"Costo: {cost}")
